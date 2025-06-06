5d5e1f58580ec1db5ff071e067a6b5da565db4c2
Al-MardasDev <94618007+dark0crystal@users.noreply.github.com>
2025-06-06 11:16:18 +0400

    Update math.go , add new functions

--- testing/math.go
+++ testing/math.go
@@ -9,3 +9,13 @@ func Subtract(a, b int) int {
 	return a - b
 }
 
+func Multiply(a, b int) int {
+	return a * b
+}
+
+func Divide(a, b int) (int, error) {
+	if b == 0 {
+		return 0, errors.New("cannot divide by zero")
+	}
+	return a / b, nil
+}
